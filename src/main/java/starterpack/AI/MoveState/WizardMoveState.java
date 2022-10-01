09d8ea94248fdebce7daf31ef50d81e28c7384e3
erin-yiran-wang <erinwang27y@gmail.com>
2022-10-01 14:28:21 -0500

    wizardMoveState set slope go to center

--- src/main/java/starterpack/AI/MoveState/WizardMoveState.java
+++ src/main/java/starterpack/AI/MoveState/WizardMoveState.java
@@ -42,28 +42,37 @@ public class WizardMoveState extends IMoveState{
         if(ydiff + xdiff <= Utils.GetSpeed(this)) return kbest;
         int x = cPos.getX();
         int y = cPos.getY();
-        int newx = 4;
-        int newy = 4;
-        if(x < 4 && y < 4) {
-            newx = Utils.GetSpeed(this) / 2 + x;
-            newy = Utils.GetSpeed(this) - newx + y;
-        } else if(x >5 && y < 4 ) {
-            newx = x - Utils.GetSpeed(this) / 2;
-            newy = Utils.GetSpeed(this) - newx + y;
-        } else if(x < 4 && y > 5) {
-            newx = x + Utils.GetSpeed(this) / 2;
-            newy = y - (Utils.GetSpeed(this) - newx);
+        // int newx = 4;
+        // int newy = 4;
+        // if(x <= 4 && y <= 4) {
+        //     newx = Utils.GetSpeed(this) / 2 + x;
+        //     newy = Utils.GetSpeed(this) - newx + y;
+        // } else if(x >=5 && y <= 4 ) {
+        //     newx = x - Utils.GetSpeed(this) / 2;
+        //     newy = Utils.GetSpeed(this) - newx + y;
+        // } else if(x <= 4 && y >= 5) {
+        //     newx = x + Utils.GetSpeed(this) / 2;
+        //     newy = y - (Utils.GetSpeed(this) - newx);
+        // } else {
+        //     newx = x - Utils.GetSpeed(this) / 2;
+        //     newy = y - (Utils.GetSpeed(this) - newx);
+        // }
+        double slope = Math.abs(ydiff / (double) xdiff);
+        int rawYMov = 0;
+        int rawXMov = 0;
+        if(Math.abs(slope)>=1) {
+            rawYMov = Math.min(ydiff, (int)Math.round(ydiff / (double) (xdiff + ydiff) * Utils.GetSpeed(this)));
+            rawXMov = Math.min(xdiff, Utils.GetSpeed(this) - rawYMov);
         } else {
-            newx = x - Utils.GetSpeed(this) / 2;
-            newy = y - (Utils.GetSpeed(this) - newx);
+            rawXMov = Math.min(ydiff, (int)Math.round(xdiff / (double) (xdiff + ydiff) * Utils.GetSpeed(this)));
+            rawYMov = Math.min(xdiff, Utils.GetSpeed(this) - rawXMov);
         }
-        // double slope = Math.abs(ydiff / (double) xdiff);
-        // int rawYMov = Math.min(ydiff, (int)Math.round(slope * Utils.GetSpeed(this)));
+        
         // int rawXMov = Math.min(xdiff, Utils.GetSpeed(this) - rawYMov);
-        // if(cPos.getY() > kbest.getY()) rawYMov *= -1;
-        // if(cPos.getX() > kbest.getX()) rawXMov *= -1;
-        // Position idealPosChange = new Position(rawXMov, rawYMov);
-        Position idealPosChange = new Position(newx, newy);
+        if(cPos.getY() > kbest.getY()) rawYMov *= -1;
+        if(cPos.getX() > kbest.getX()) rawXMov *= -1;
+        Position idealPosChange = new Position(rawXMov, rawYMov);
+        // Position idealPosChange = new Position(newx, newy);
         return idealPosChange;
         
     }
