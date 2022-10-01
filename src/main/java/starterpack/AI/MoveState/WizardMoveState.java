985cf174b5fbf877f363107b0dc8a8a48f26b93d
erin-yiran-wang <erinwang27y@gmail.com>
2022-10-01 13:22:19 -0500

    added wizardmovestate and wizardAttack

--- src/main/java/starterpack/AI/MoveState/WizardMoveState.java
+++ src/main/java/starterpack/AI/MoveState/WizardMoveState.java
@@ -2,7 +2,7 @@ package starterpack.AI.MoveState;
 
 import starterpack.game.GameState;
 import starterpack.game.Position;
-
+import starterpack.AI.Utils.*;;
 public class WizardMoveState extends IMoveState{
 
     public WizardMoveState(GameState gameState, int playerIndex) {
@@ -12,13 +12,42 @@ public class WizardMoveState extends IMoveState{
 
     @Override
     public Position Update() {
-        return new Position(0,0);
+        // return new Position(0,0);
+        Position tele = Teleport();
+        if (tele != null) return tele;
+        return Move();
         
     }
 
     @Override
     public Position Move() {
-        return new Position(0,0);
+        Position cPos = Utils.GetPosition(this, getPlayerIndex());
+        Position kbest = null;
+        switch (getPlayerIndex()){
+            case 0:
+            kbest = new Position(4,4);
+            break;
+            case 1:
+            kbest = new Position(5,4);
+            break;
+            case 2:
+            kbest = new Position(4,5);
+            break;
+            case 3:
+            kbest = new Position(5,5);
+            break;
+        }
+        int ydiff = Math.abs(kbest.getY() - cPos.getY());
+        int xdiff = Math.abs(kbest.getX() - cPos.getX());
+        if(ydiff + xdiff <= Utils.GetSpeed(this)) return kbest;
+        
+        double slope = Math.abs(ydiff / (double) xdiff);
+        int rawYMov = Math.min(ydiff, (int)Math.round(slope * Utils.GetSpeed(this)));
+        int rawXMov = Math.min(xdiff, Utils.GetSpeed(this) - rawYMov);
+        if(cPos.getY() > kbest.getY()) rawYMov *= -1;
+        if(cPos.getX() > kbest.getX()) rawXMov *= -1;
+        Position idealPosChange = new Position(rawXMov, rawYMov);
+        return idealPosChange;
         
     }
 
