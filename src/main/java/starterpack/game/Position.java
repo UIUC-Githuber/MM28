4096259806878eb2194b3ef9cf3da319a48fdd10
ETwilight <hellobrian18@163.com>
2022-09-30 19:11:15 -0500

    Cloned

---
+++ src/main/java/starterpack/game/Position.java
@@ -0,0 +1,47 @@
+package starterpack.game;
+
+public class Position {
+  private int x;
+  private int y;
+
+  public Position() {}
+
+  public Position(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public void translate(Position destination) {
+    this.x = destination.getX();
+    this.y = destination.getY();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
+    }
+    if (!(obj instanceof Position)) {
+      return false;
+    }
+    Position p = (Position) obj;
+    // If the individual coordinates are the same
+    return ((this.getX() == p.getX()) & (this.getY() == p.getY()));
+  }
+}
---
+++ src/main/java/starterpack/strategy/StrategyConfig.java
@@ -0,0 +1,15 @@
+package starterpack.strategy;
+
+public class StrategyConfig {
+
+    /**
+     * Return the strategy that your bot should use.
+     * @param playerIndex A player index that can be used if necessary.
+     * @return A Strategy object.
+     */
+    public static Strategy getStrategy(int playerIndex) {
+//        if (playerIndex == 0) return new OnlyBuyAndUseShieldStrategy();
+//        else return new OnlyGoto00AndAttackStrategy();
+        return new RandomStrategy();
+    }
+}
